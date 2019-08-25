435c2328215bc828d6aa54fb984610172b89d5dd
Kaitlyn Steagall <kaitlynsteagall@gmail.com>
2019-08-24 23:01:01 -0700

    change spacing of buttons and make them bootstrap btns. Also add AJAX funtion that displays GIFS on click of the btn

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,11 +1,11 @@
 // array of animals that load on page startup. User input will be pushed into this array.
-var animalArray = ["Ferret", "Dog", "Cat", "Fish", "Otter", "Tiger"];
+var animalArray = ["ferret", "dog", "cat", "fish", "otter", "tiger"];
 
 //function to display the buttons in the array
 function displayButton() {
   $("#animalButtons").empty();
   for (var i = 0; i < animalArray.length; i++) {
-    var button = $("<button>").attr("data-name", animalArray[i]).addClass("animalButton").text(animalArray[i]);
+    var button = $("<button class='btn mr-2'>").attr("data-animal", animalArray[i]).addClass("animalButton").text(animalArray[i]);
     $("#animalButtons").append(button);
   }
 }
@@ -13,24 +13,35 @@ function displayButton() {
 // On click event for when "submit" is clicked: add user input to the array(which will then be displayed using)
 $("#addAnimal").on("click", function(event) {
   event.preventDefault();
-  var animal = $("#animal").val().trim();
+  var animal = $("#animal").val().trim().toLowerCase();
   animalArray.push(animal);
   displayButton();
 });
 
-// function displayAnimalGIF() {
+//function to display GIFs from GIPHY API
+function displayAnimalGIF() {
 
-//   var animal = $(this).attr("data-animal");
-//   var queryURL = "https://api.giphy.com/v1/gifs/trending?q=" + animal + "api_key=nM33hY6owuDXKqa1jMH0g8lR05UiqbVL&limit=10";
-//   $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).then(function (response) {
-//       $("#animalGIF").text(response);
-//     });
-// };
+  var animal = $(this).attr("data-animal");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=nM33hY6owuDXKqa1jMH0g8lR05UiqbVL&limit=10";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+      var results = response.data;
+
+      for (var i = 0; i < results.length; i++){
+      var GIF = $("<div class='float-left mr-3'>");
+      var pTag = $("<p>").text("Rating: " + results[i].rating)
+      var animalImg = $("<img class='mb-5'>").attr("src", results[i].images.fixed_height.url);
+
+      GIF.append(pTag);
+      GIF.append(animalImg);
+      $("#animalGIF").prepend(GIF);
+      }
+    });
+};
 
 
-// $(document).on("click", ".animalButton", displayAnimalGIF);
+$(document).on("click", ".animalButton", displayAnimalGIF);
 
 displayButton();
